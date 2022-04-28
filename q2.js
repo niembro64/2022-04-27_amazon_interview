064d140b432c1bf8092e8e8cfe09bebda20dd4aa
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-04-28 01:17:38 -0400

    az questions

---
+++ q2.js
@@ -0,0 +1,47 @@
+const getMaxFreq = (s) => {
+  var max = 0;
+  var temp = 0;
+  for (var i = 0; i < s.length; i++) {
+    temp++;
+    if (s[i] != s[i + 1]) {
+      max = temp > max ? temp : max;
+      temp = 0;
+    }
+  }
+  return max;
+};
+
+const getMinFreq = (s) => {
+  var min = 9999999999999;
+  var temp = 0;
+  for (var i = 0; i < s.length; i++) {
+    temp++;
+    if (s[i] != s[i + 1]) {
+      min = temp < min ? temp : min;
+      temp = 0;
+    }
+  }
+  return min;
+};
+
+const getFreqDev = (s) => {
+  var maxFreqDev = 0;
+  var temp = 0;
+  for (var i = 0; i < s.length; i++) {
+    for (var j = i; j < s.length; j++) {
+      temp = getMaxFreq(s.substring(i, j)) - getMinFreq(s.substring(i, j));
+      maxFreqDev = temp > maxFreqDev ? temp : maxFreqDev;
+    }
+  }
+
+  return maxFreqDev;
+};
+
+s = ["aaabbbcccz", "abbcccddddz", "abaabbaaabbbbz"];
+
+for (var i = 0; i < s.length; i++) {
+  // console.log(getFreqDev(s[i]));
+  // console.log(getMaxFreq(s[i]));
+  // console.log(getMinFreq(s[i]));
+  console.log(getFreqDev(s[i]));
+}
